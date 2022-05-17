c3fbe1a81389cb39418bf4932997fe3c6d97c190
Chencircleyuan <105686620+Chencircleyuan@users.noreply.github.com>
2022-05-17 12:01:18 +0800

    Add files via upload

--- app.py
+++ app.py
@@ -1,13 +1,14 @@
+from distutils.log import debug
+from pickle import TRUE
 from flask import Flask
-
 app= Flask(__name__)
 
 @app.route('/')
 def index():
-    return "Hello World"
+    return render_templates('index.html',name=name) 
+    app.send_ststic_
+    index.html
+    return "<h1>Hello World</h1>"
 
 if __name__=='__main__':
-    app.run()
-
-
-
+    app.run(port=5050,debug=TRUE)
